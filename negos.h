20d10026c66c41ce2e77fefceb4b5793e8a53aff
FeR12J <FeR@BigFeR.local>
2015-04-17 12:01:44 +0200

    negos preliminar con fallo

---
+++ negos.h
@@ -0,0 +1,43 @@
+#ifndef NEGOS_H
+#define NEGOS_H
+
+#include <QString>
+#include "fecha.h"
+
+class negos
+{
+
+
+private:
+    QString origen;
+    QString destino;
+    int id;
+    int numeroPlazas;
+    Fecha fecha;
+
+//MIRAR LO DE CONST Y SUICIDARSE UN POCO
+public:
+    negos();
+    negos(QString, QString, int, int, Fecha);
+    ~negos();
+
+     void setNego(QString origen, QString destino, int id, int numeroPlazas, Fecha fecha);
+
+
+     void setOrigen(QString origen);
+     void setDestino(QString destino);
+     void setId(int id);
+     void setNumeroPlazas(int numeroPlazas);
+     void setFecha(Fecha fecha);
+
+     QString getOrigen();
+     QString getDestino();
+     int getId();
+     int getNumeroPlazas();
+     Fecha getFecha();
+
+     negos getNego(QString origen, QString destino, int id, int numeroPlazas, Fecha fecha);
+
+};
+
+#endif // NEGOS_H
