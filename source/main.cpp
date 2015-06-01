7ef5d3367c43caff84e6b6f33e7cbce53a30fb87
Hugo Ferrando Seage <hugoseage@gmail.com>
2015-06-01 19:52:57 +0200

    Catch para Unit Tests

--- source/main.cpp
+++ source/main.cpp
@@ -8,11 +8,25 @@
  *  @author Sergio Candel
  *  Website: https://github.com/hugo19941994/ViajeFacil
  */
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include <QApplication>
 #include "./mainWindow.hpp"
+#include "./catch/catch.hpp"
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
 
 int main(int argc, char *argv[]) {
+    int result = Catch::Session().run( argc, argv );
     QApplication a(argc, argv);
     mainWindow w;
     w.show();
