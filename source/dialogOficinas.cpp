2510939150362d1193734e713901ed9ecc45e2f6
hugo19941994 <hugoseage@gmail.com>
2015-06-22 14:27:30 -0400

    Jenkins: Updated docs

--- source/dialogOficinas.cpp
+++ source/dialogOficinas.cpp
@@ -6,6 +6,7 @@
 #include "./ui_dialogOficinas.h"
 #include "./pelVector.hpp"
 #include "./dialogOficinas.hpp"
+#include <QMessageBox>
 
 dialogOficinas::dialogOficinas(QWidget *parent) :
     QDialog(parent), ui(new Ui::dialogOficinas) {
@@ -39,10 +40,53 @@ Oficina dialogOficinas::crear() {
                 ui->lineCont->text().toStdString()};
 }
 
+void dialogOficinas::accept(){
+    if (maybeSave) {
+        done(1);  // Termina y emite accepted
+    }
+}
+
 void dialogOficinas::on_buttonBox_accepted() {
-    if (editando) {
+   /* if (editando) {
         oficinaAEditar->setNombre(ui->lineNombre->text().toStdString());
         oficinaAEditar->setPais(ui->linePais->text().toStdString());
         oficinaAEditar->setContinente(ui->lineCont->text().toStdString());
+    }*/
+
+    maybeSave = true;
+        if (editando) {
+            if (ui->lineNombre->text().isEmpty()) {
+                QMessageBox::information(this, "",QString ("Nombre no puede ser vacío"));
+                maybeSave = false;
+                accept();
+            }
+
+            if (ui->linePais->text().isEmpty()) {
+                QMessageBox::information(this, "",QString ("Pais no puede ser vacío"));
+                maybeSave = false;
+                accept();
+            }
+
+
+            //Solo falta esta parte, los demas casos funcionan
+
+            /*for(auto &iter : listaOw->at(1)->getOficinas()){
+                if (ui->lineNombre->text().toStdString() == iter->getNombre()) {
+                    QMessageBox::information(this, "",QString ("Ya hay una oficina con este nombre"));
+                    maybeSave = false;
+                    accept();
+                }
+            }*/
+
+            if (maybeSave) {
+                oficinaAEditar->setNombre(ui->lineNombre->text().toStdString());
+                maybeSave = true;
+                accept();
+            }
+        }
+        else {  // Creando una oficina nuevo
+            maybeSave = true;
+            accept();
+        }
     }
-}
+
